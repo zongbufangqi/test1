4cfa6417d2fa75e789f83b02d1424b3ad923c73b
Administrator <Administrator@JQJLJDG6B4V3H09>
2019-08-27 20:12:27 +0800

    df

---
+++ sp01-commons/src/main/java/com/tedu/sp01/service/ItemService.java
@@ -0,0 +1,14 @@
+package com.tedu.sp01.service;
+
+import java.util.List;
+
+import com.tedu.sp01.pojo.Item;
+
+public interface ItemService {
+	List<Item> getItems(String orderId);
+	/**
+	 * 保存订单时，减少商品库存
+	 * @param list
+	 */
+	void decreaseNumbers(List<Item> list);
+}
